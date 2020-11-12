37d8ecf5efd228b0cd253c445aa68fb3f2b91263
deepikasampangi <deepikasampangi@gmail.com>
2020-11-12 18:59:28 +0530

    jsx transform

---
+++ src/app.jsx
@@ -0,0 +1,6 @@
+const element = (
+    <div title="Outer_div">
+        <h1>Hello There From JSX using Express App, from a jsx file</h1>    
+    </div>
+);
+ReactDOM.render(element, document.getElementById('content'));
