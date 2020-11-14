b313e1534a676e982c022a73cd2b8049af1e9c1b
deepikasampangi <deepikasampangi@gmail.com>
2020-11-14 15:59:49 +0530

    passing params using props

--- src/app.jsx
+++ src/app.jsx
@@ -10,10 +10,32 @@ class IssueFilter extends React.Component{
 
 class IssueTable extends React.Component{
   render(){
+    const rowStyle = {border: "1px solid silver", padding: 4};
     return(
-      <div>
-        Place holder for Issue Table
-      </div>
+      <table style = {{borderCollapse: "collapse"}}>
+        <thead>
+          <tr>
+            <th style = {rowStyle}>ID</th>
+            <th style = {rowStyle}>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          <IssueRow rowStyle={rowStyle} issue_id={1} issue_title={"Error in console when clicking add"} />
+          <IssueRow rowStyle={rowStyle} issue_id={2} issue_title={"JS is just too much"} />
+        </tbody>
+      </table>
+    );
+  }
+}
+
+class IssueRow extends React.Component{
+  render(){
+    const style = this.props.rowStyle;
+    return(
+      <tr>
+        <td style = {style}>{this.props.issue_id}</td>
+        <td style = {style}>{this.props.issue_title}</td>
+      </tr>
     );
   }
 }
